3a6aa46d2907957e223eed250faedca04cc44f5b
Pavel Šmejkal <smejkal.pa@gmail.com>
2019-10-20 22:30:53 +0200

    Cmake to Ubuntu only for now

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,9 +1,43 @@
-#include "SDL/SDL.h"
+#include "SDL2/SDL.h"
 
 #include <iostream>
 
 int main() {
-    SDL_Event e;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL init failded: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Hello world", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "SDL window creation failded: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        SDL_DestroyWindow(window);
+        std::cerr << "SDL renderer creation failed: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+    bool quit = false;
+
+    while (!quit) {
+        while(SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
 
     std::cout << "Done" << std::endl;
 
