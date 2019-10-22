4a241c815133020a22fd91fc4cb890893ec858ca
Pavel Šmejkal <smejkal.pa@gmail.com>
2019-10-22 21:27:16 +0200

    Implement the most basic texture rendering

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,9 +1,12 @@
-#include "SDL2/SDL.h"
 
 #include "ecs.h"
 #include "math/vec2.h"
 
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
 #include <iostream>
+#include <string>
 
 namespace eg {
 
@@ -16,10 +19,52 @@ struct PositionComponent {
 };
 
 struct SpriteComponent {
-    float Dummy;
+    SDL_Texture* Texture;
 };
 
+SDL_Texture* loadTexture(const std::string& path) {
+    // TODO(pavel): Maintain a texture registry - do not load duplicate textures
+    SDL_Surface* loadingSurface = IMG_Load(path.c_str());
+    if (!loadingSurface) {
+        std::cerr << "Failed loading texture " << path << ", error: " << IMG_GetError() << std::endl;
+        return nullptr;
+    }
+
+    auto render = ECS::reg().raw<RendererComponent>();
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(render->Renderer, loadingSurface);
+    SDL_FreeSurface(loadingSurface);
+    
+    return texture;
+}
+
+void initMap() {
+    constexpr int WIDTH = 12;
+    constexpr int HEIGHT = 9;
+    constexpr int TILE_SIZE = 64;
+
+    for (int y = 0; y < HEIGHT; ++y)
+    {
+        for (int x = 0; x < WIDTH; ++x)
+        {
+            std::string texturePath = "textures/floor.png";
+            if (x == 0 || y == 0 || x == WIDTH - 1 || y == HEIGHT - 1)
+                texturePath = "textures/wall.png";
+
+            SDL_Texture* texture = loadTexture(texturePath);
+            if (!texture)
+                continue;
+
+            auto [ent, pos, sprite] = ECS::reg().create<PositionComponent, SpriteComponent>();
+            pos.Pos = Vec2(x * TILE_SIZE, y * TILE_SIZE);
+            sprite.Texture = texture;
+        }
+    }
+
+}
+
 void initGame() {
+    initMap();
+
     auto [entity, pos, sprite] = ECS::reg().create<PositionComponent, SpriteComponent>();
     pos.Pos = Vec2(100, 50);
 }
@@ -30,8 +75,9 @@ void update() {
 
     SDL_RenderClear(rc->Renderer);
 
-    ECS::reg().view<PositionComponent, SpriteComponent>().each([](auto entity, auto& pos, auto& sprite) {
-        std::cout << pos.Pos.x() << " " << pos.Pos.y() << std::endl;
+    ECS::reg().view<PositionComponent, SpriteComponent>().each([render = rc->Renderer](auto entity, auto& pos, auto& sprite) {
+        SDL_Rect dst{ int(pos.Pos.x()), int(pos.Pos.y()), 64, 64 };
+        SDL_RenderCopy(render, sprite.Texture, nullptr, &dst);
     });
 
     SDL_RenderPresent(rc->Renderer);
@@ -59,10 +105,16 @@ int runGame() {
         return 1;
     }
 
-    initGame();
+    int flags = IMG_INIT_PNG;
+    if ((IMG_Init(flags) & flags) != flags) {
+        std::cerr << "SDL IMG init failed, " << IMG_GetError() << std::endl;
+        return 1;
+    }
 
     auto rendererEntity = eg::ECS::reg().create();
     ECS::reg().get_or_assign<eg::RendererComponent>(rendererEntity).Renderer = renderer;
+
+    initGame();
 
     SDL_Event event;
     bool quit = false;
